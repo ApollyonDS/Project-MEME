e6e3d1619421a99f55d6b5688601877b5885521a
TV\Tristan <tristan.visnar@student.um.si>
2016-12-05 18:56:57 +0100

    Player health and enemy damage

--- Project M.E.M.E(1)/Assets/Sprites/Background/Portals/PortalRotate.cs
+++ Project M.E.M.E(1)/Assets/Sprites/Background/Portals/PortalRotate.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PortalRotate : MonoBehaviour {
 
     public int hitrost;
+    public string sceneName;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player") {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update () {
         transform.Rotate(Vector3.back, Time.deltaTime * hitrost );
     }
 }
